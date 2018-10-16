ed85a1dfc88cb499d1f5a1ba9e0da570ffaceff2
zhangyingwei <zhangyw001@gmail.com>
2018-10-16 20:41:20 +0800

    # 文章管理页面完成

--- src/js/treehole/treehole-article-manage.js
+++ src/js/treehole/treehole-article-manage.js
@@ -5,8 +5,7 @@ if (!manageArticleApp) {
     data: {
       initData: {
         article: {
-          status: [
-            {
+          status: [{
               name: "选择文章状态",
               value: 0
             },
@@ -29,12 +28,11 @@ if (!manageArticleApp) {
         title: "",
         statu: 0
       },
-      data: [
-        {
+      data: [{
           title: "捅破那层窗户纸，二叉树的遍历真特么简单",
           desc: "二叉树是一种非常神奇的树，由于其在查找排序等某些方面的优越性....",
           kind: "关于JAVA的一些东西",
-          tags: ["java","生活"],
+          tags: ["java", "生活"],
           allowCommont: false,
           statu: 2,
           statuText: "未发布"
@@ -43,22 +41,55 @@ if (!manageArticleApp) {
           title: "捅破那层窗户纸，二叉树的遍历真特么简单",
           desc: "二叉树是一种非常神奇的树，由于其在查找排序等某些方面的优越性....",
           kind: "关于JAVA的一些东西",
-          tags: ["java","生活"],
+          tags: ["java", "生活"],
           allowCommont: true,
           statu: 2,
           statuText: "未发布"
         }
-      ]
+      ],
+      page: {
+        current: 1,
+        pageSize: 10,
+        total: 2
+      }
     },
     methods: {
       query: function () {
         console.log(this.search)
       },
-      editLine: function(line){
+      editLine: function (line) {
         console.log(line)
       },
-      pageList: function() {
+      pageList: function () {
         return new Array(this.data.length)
+      },
+      getPageType: function (index) {
+        const currentPage = this.page.current - 1
+        if (index === currentPage) {
+          return "active"
+        } else {
+          return ""
+        }
+      },
+      pageChange: function(index){
+        this.page.current = index + 1
+      },
+      nextPage: function() {
+        if(this.page.current < this.getTotalPage()){
+          this.page.current +=1
+        }
+      },
+      prePage: function() {
+        if(this.page.current > 1){
+          this.page.current -= 1
+        }
+      },
+      getTotalPage: function() {
+        if(this.page.total%this.page.pageSize == 0){
+          return this.page.total / this.page.pageSize
+        }else {
+          return (this.page.total / this.page.pageSize) + 1
+        }
       }
     },
     mounted: function () {
