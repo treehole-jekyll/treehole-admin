41446ccef4ee10ba0bb4e2ec1c2438cb8cabec25
zhangyingwei <zhangyw_001@126.com>
2018-10-16 11:44:04 +0800

    # 删除 filepuload 与 editor js脚本 # 把图片上传于编辑器合到vue中

--- src/js/treehole/treehole-article-create.js
+++ src/js/treehole/treehole-article-create.js
@@ -1,11 +1,12 @@
 var app = undefined
-if (!app){
+if (!app) {
   app = new Vue({
     el: '#treehole-article-create',
     data: {
       msg: "test",
-      kinds: [
-        {
+      editor: undefined,
+      imageList: [],
+      kinds: [{
           name: "请选择",
           value: 0
         },
@@ -32,14 +33,125 @@ if (!app){
         console.log("save article")
         console.log(this.article)
       },
-      publish: function() {
+      publish: function () {
         console.log("public article")
       },
-      initEditor: function() {
+      initEditor: function () {
+        this.editor = new mditor(document.getElementById('thole-editor'), {
+          previewClass: 'treehole-article'
+        })
+        this.bindEditorKeys()
+      },
+      bindEditorKeys: function () {
+        var ctrlDown = false;
+        var sDown = false;
+        const self = this
+        $("#thole-editor").bind("keydown", function (event) {
+          var k = event.which;
+          const ctrlK = self.getOsType()
+          if (k === ctrlK) {
+            ctrlDown = true
+            event.preventDefault()
+          }
+          if (ctrlDown) {
+            event.preventDefault()
+            if (k == 83) {
+              self.funCtrlS()
+            }
+          }
+        });
+        $("#thole-editor").bind("keyup", function (event) {
+          var k = event.which;
+          const ctrlK = self.getOsType()
+          if (k === ctrlK) {
+            ctrlDown = false
+          }
+        });
+      },
+      getOsType: function () {
+        const agent = navigator.userAgent
+        if (agent.indexOf("Mac") != -1) {
+          return 91
+        } else {
+          return 17
+        }
+      },
+      funCtrlS: function () {
+        console.log("ctrl s")
+      },
+      inputImage: function () {
+        const self = this
+        const files = $('#fileload')[0].files
+        const filesList = []
+        for (let i = 0; i < files.length; i++) {
+          filesList.push(files[i])
+        }
+        self.imageList = filesList.filter(ele => self.fileTypeFilter(ele)).filter(ele => self.fileSizeFilter(ele))
+        self.imageList.forEach(ele => {
+          self.previewImage(ele)
+        })
+      },
+      fileTypeFilter: function(file) {
+        const result = $.inArray(file.type, ["image/png", "image/gif", "image/jpg", "image/jpeg"]) >= 0
+        if (!result) {
+          console.log("file type not accepted", file.type)
+        }
+        return result
+      },
+      fileSizeFilter: function (file) {
+        const result = file.size < 2014 * 1024 * 100
+        if (!result) {
+          console.log("file size not accepted", file.size)
+        }
+        return result
+      },
+      previewImage: function (file) {
+        const reader = new FileReader();
+        const warpperWidth = $("#upload-warpper").width()
+        const warpperHeight = $("#upload-warpper").height()
+        console.log(warpperHeight)
+        reader.readAsDataURL(file);
+        reader.onload = function (e) {
+          url = reader.result
+          const img = new Image()
+          img.src = url
+          const width = img.width
+          const height = img.height
+          let size = ""
+          if (width > height) {
+            size = "auto 100%"
+          } else {
+            size = "100% auto"
+          }
+          $("#upload-warpper")
+            .css("background-image", 'url("' + url + '")')
+            .css('background-size', "100%")
+            .css('background-repeat', 'no-repeat')
+            .css('background-position', 'top')
 
+          $(".upload-text").hide()
+        }
+      },
+      submitImageTemplate: function () {
+        //提交图片实例
+        //TODO
+        var formData = new FormData();
+        this.imageList.forEach(ele => {
+          formData.append("upfile[]", ele);
+        })
+
+        $.ajax({
+          url: "1.php",
+          type: "POST",
+          data:formData,
+          cache:false,         //不设置缓存
+          processData: false,  // 不处理数据
+          contentType: false   // 不设置内容类型
+    });
       }
     },
-    mounted: function(){
+    mounted: function () {
+      this.initEditor()
       $("#input-tags").tagsinput();
     }
   })
